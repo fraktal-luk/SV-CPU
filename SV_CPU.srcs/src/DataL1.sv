431efc2adf0e5cd8d7d39b02d54774cf2a643aab
fraktal-luk <fraktal3725@gmail.com>
2025-03-07 19:05:35 +0100

    Reorganize/refac data cache

--- SV_CPU.srcs/src/DataL1.sv
+++ SV_CPU.srcs/src/DataL1.sv
@@ -22,10 +22,10 @@ module DataL1(
     localparam int DATA_TLB_SIZE = 32;
     
         localparam logic DONT_TRANSLATE = 1; // TMP
-        
-    typedef Mbyte DataBlock[BLOCK_SIZE];
 
 
+    AccessInfo accesses[N_MEM_PORTS];
+    Translation translations[N_MEM_PORTS]; 
 
     logic notifyFill = 0;
     Mword notifiedAdr = 'x;
@@ -35,6 +35,21 @@ module DataL1(
 
     int uncachedCounter = -1;
     logic uncachedBusy = 0;
+    
+
+    typedef Mbyte DataBlock[BLOCK_SIZE];
+
+
+
+    // Simple array for simple test cases, without blocks, transaltions etc
+    Mbyte staticContent[PAGE_SIZE]; // So far this corresponds to way 0
+    
+    localparam Mword UNCACHED_BASE = 'h80000000;
+    Mbyte uncachedArea[PAGE_SIZE];
+    
+
+    // Data and tag arrays
+    PhysicalAddressHigh tagsForWay[BLOCKS_PER_WAY] = '{default: 0}; // tags for each block of way 0
     
 
     // CAREFUL: below only for addresses in the range for data miss tests 
@@ -47,46 +62,227 @@ module DataL1(
     int       mappingFillCounters[Mword];
     Mword     readyMappingsToFill[$];
 
-        
-        typedef struct {
-            logic ongoing = 0;
-            logic ready = 0;
-            Mword adr = 'x;
-            Mword data = 'x;
-            AccessSize size = SIZE_NONE;
-            int counter = -1;
-        } UncachedRead;
-
-        UncachedRead uncachedReads[N_MEM_PORTS]; // Should be one (ignore other than [0])
-
-
-    // Data and tag arrays
-    PhysicalAddressHigh tagsForWay[BLOCKS_PER_WAY] = '{default: 0}; // tags for each block of way 0
-
-    // Simple array for simple test cases, without blocks, transaltions etc
-    Mbyte content[4096]; // So far this corresponds to way 0
-    
-    localparam Mword UNCACHED_BASE = 'h80000000;
-    Mbyte uncachedArea[4096];
-
-
-    AccessInfo accesses[N_MEM_PORTS];
-    Translation translations[N_MEM_PORTS];        
-
 
 
     function automatic logic isUncachedRange(input Mword adr);
-        return adr[31];
+        return //adr[31];
+               adr >= UNCACHED_BASE && adr < UNCACHED_BASE + $size(uncachedArea);
     endfunction
 
     function automatic logic isStaticDataRange(input Mword adr);
-        return adr < $size(content);
+        return adr < $size(staticContent);
     endfunction
 
     function automatic logic isStaticTlbRange(input Mword adr);        
         return isUncachedRange(adr) // TEMP: uncached region is mapped by default
                 || adr < 'h80000; // TEMP: Let's give 1M for static mappings
     endfunction
+
+
+
+    //    typedef struct {
+    //        logic active;
+    //            logic store;            
+    //            logic uncachedReq;
+    //        Mword adr;
+    //        AccessSize size;
+    //    } DataReadReq;
+    
+    //    localparam DataReadReq EMPTY_READ_REQ = '{0, 0, 0, 'x, SIZE_NONE};
+   
+        typedef struct {
+            logic ready = 0;
+        
+                logic ongoing = 0;
+                Mword adr = 'x;
+                AccessSize size = SIZE_NONE;
+            
+            Mword data = 'x;
+
+            int counter = -1;
+        } UncachedRead;
+
+        UncachedRead uncachedReads[N_MEM_PORTS]; // Should be one (ignore other than [0])
+
+
+
+
+    function automatic void reset();
+        staticContent = '{default: 0};
+        uncachedArea = '{default: 0};
+        tagsForWay = '{default: 0};
+        
+        filledBlocks.delete();
+        blockFillCounters.delete();
+        readyBlocksToFill.delete();
+        
+        filledMappings.delete();
+        mappingFillCounters.delete();
+        readyMappingsToFill.delete();
+        
+            uncachedCounter = -1;
+            uncachedBusy = 0;
+    endfunction
+
+
+    task automatic handleWrites();
+        doWrite(TMP_writeReqs[0]);
+    endtask
+
+        
+    class PageWriter#(type Elem = Mbyte, int ESIZE = 1, int BASE = 0);
+        static
+        function automatic void writeTyped(ref Mbyte arr[PAGE_SIZE], input Mword adr, input Elem val);
+            Mbyte wval[ESIZE] = {>>{val}};
+            arr[(adr - BASE) +: ESIZE] = wval;
+        endfunction
+        
+        static
+        function automatic Elem readTyped(ref Mbyte arr[PAGE_SIZE], input Mword adr);                
+            Mbyte chosen[ESIZE] = arr[(adr - BASE) +: ESIZE];
+            Elem wval = {>>{chosen}};
+            return wval;
+        endfunction
+    endclass
+
+
+
+    ////////////////////////////////////
+    // Specific write & read functions
+
+    function automatic void writeToStaticRangeW(input Mword adr, input Mword val);
+        PageWriter#(Word, 4)::writeTyped(staticContent, adr, val);
+    endfunction
+
+    function automatic void writeToStaticRangeB(input Mword adr, input Mbyte val);
+        PageWriter#(Mbyte, 1)::writeTyped(staticContent, adr, val);
+    endfunction
+
+
+    function automatic void writeToUncachedRangeW(input Mword adr, input Mword val);
+        PageWriter#(Word, 4, UNCACHED_BASE)::writeTyped(uncachedArea, adr, val);
+    endfunction
+
+    function automatic void writeToUncachedRangeB(input Mword adr, input Mbyte val);
+        PageWriter#(Mbyte, 1, UNCACHED_BASE)::writeTyped(uncachedArea, adr, val);
+    endfunction
+
+
+    function automatic Mword readWordStatic(input Mword adr);
+        return PageWriter#(Word, 4)::readTyped(staticContent, adr);
+    endfunction
+
+    function automatic Mword readByteStatic(input Mword adr);
+        return Mword'(PageWriter#(Mbyte, 1, UNCACHED_BASE)::readTyped(staticContent, adr));
+    endfunction
+
+
+    function automatic Mword readWordUncached(input Mword adr);
+        return PageWriter#(Word, 4)::readTyped(uncachedArea, adr);
+    endfunction
+
+    function automatic Mword readByteUncached(input Mword adr);
+        return Mword'(PageWriter#(Mbyte, 1, UNCACHED_BASE)::readTyped(uncachedArea, adr));
+    endfunction
+
+
+
+    function automatic void writeToDynamicRangeW(input Mword adr, input Mword val);
+        localparam int ACCESS_SIZE = 4;
+        
+        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
+        PhysicalAddressLow physLow = adr % BLOCK_SIZE;
+
+        Mbyte wval[ACCESS_SIZE] = {>>{val}};
+        filledBlocks[physBlockBase][physLow +: ACCESS_SIZE] = wval;
+    endfunction
+
+    function automatic void writeToDynamicRangeB(input Mword adr, input Mbyte val);
+        localparam int ACCESS_SIZE = 1;
+        
+        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
+        PhysicalAddressLow physLow = adr % BLOCK_SIZE;
+
+        Mbyte wval[ACCESS_SIZE] = {>>{val}};
+        filledBlocks[physBlockBase][physLow +: ACCESS_SIZE] = wval;
+    endfunction
+
+
+    function automatic Mword readWordDynamic(input DataBlock block, input int offset);
+        localparam int ACCESS_SIZE = 4;
+
+        Mbyte chosenWord[ACCESS_SIZE] = block[offset +: ACCESS_SIZE];
+        Mword wval = {>>{chosenWord}};
+
+        return (wval);
+    endfunction
+
+    function automatic Mword readByteDynamic(input DataBlock block, input int offset);
+        localparam int ACCESS_SIZE = 1;
+
+        Mbyte chosenWord[ACCESS_SIZE] = block[offset +: ACCESS_SIZE];
+        Mbyte wval = {>>{chosenWord}};
+
+        return (wval);
+    endfunction
+
+    /////////////////////////////////////
+
+
+
+    ////////////////////////////////////
+    // Presence & allocation function 
+    //
+    function automatic void allocInDynamicRange(input Mword adr);
+        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;        
+        filledBlocks[physBlockBase] = '{default: 0};            
+    endfunction
+
+    function automatic void allocInTlb(input Mword adr);
+        Translation DUMMY; 
+        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
+        
+        filledMappings[pageBase] = DUMMY;            
+    endfunction
+
+
+    function automatic logic isPhysPresent(input Mword adr);
+        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
+        return isUncachedRange(adr) || isStaticDataRange(adr) || filledBlocks.exists(physBlockBase);
+    endfunction
+
+    function automatic logic isPhysPending(input Mword adr);
+        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
+        return blockFillCounters.exists(physBlockBase);
+    endfunction
+    
+
+    function automatic logic isTlbPresent(input Mword adr);
+        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
+        return isStaticTlbRange(adr) || filledMappings.exists(pageBase);
+    endfunction
+
+    function automatic logic isTlbPending(input Mword adr);
+        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
+        return mappingFillCounters.exists(pageBase);
+    endfunction
+    
+    
+
+    function automatic void scheduleBlockFill(input Mword adr);
+        Mword physBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
+
+        if (!blockFillCounters.exists(physBase))
+            blockFillCounters[physBase] = 15;            
+    endfunction
+
+    function automatic void scheduleTlbFill(input Mword adr);
+        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
+
+        if (!mappingFillCounters.exists(pageBase))
+            mappingFillCounters[pageBase] = 12;  
+    endfunction
+
 
 
 
@@ -152,178 +348,13 @@ module DataL1(
         end
     endtask
 
-
-    task automatic handleFills();
-        handleBlockFills();
-        handleTlbFills();
-            
-        handleUncachedData();
-    endtask
-
-
-
-    function automatic void scheduleBlockFill(input Mword adr);
-        Mword physBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-
-        if (!blockFillCounters.exists(physBase))
-            blockFillCounters[physBase] = 15;            
-    endfunction
-
-    function automatic void scheduleTlbFill(input Mword adr);
-        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
-
-        if (!mappingFillCounters.exists(pageBase))
-            mappingFillCounters[pageBase] = 12;  
-    endfunction
-
-
-    function automatic void reset();
-        content = '{default: 0};
-        uncachedArea = '{default: 0};
-        tagsForWay = '{default: 0};
-        
-        filledBlocks.delete();
-        blockFillCounters.delete();
-        readyBlocksToFill.delete();
-        
-        filledMappings.delete();
-        mappingFillCounters.delete();
-        readyMappingsToFill.delete();
-        
-            uncachedCounter = -1;
-            uncachedBusy = 0;
-    endfunction
-
-
-    task automatic handleWrites();
-        doWrite(TMP_writeReqs[0]);
-    endtask
-
-        
-    class PageWriter#(type Elem = Mbyte, int ESIZE = 1, int BASE = 0);
-        static
-        function automatic void writeTyped(ref Mbyte arr[4096], input Mword adr, input Elem val);
-            Mbyte wval[ESIZE] = {>>{val}};
-            arr[(adr - BASE) +: ESIZE] = wval;
-        endfunction
-        
-        static
-        function automatic Elem readTyped(ref Mbyte arr[4096], input Mword adr);                
-            Mbyte chosen[ESIZE] = arr[(adr - BASE) +: ESIZE];
-            Elem wval = {>>{chosen}};
-            return wval;
-        endfunction
-    endclass
-
-
-    function automatic void writeToStaticRangeW(input Mword adr, input Mword val);
-        PageWriter#(Word, 4)::writeTyped(content, adr, val);
-    endfunction
-
-    function automatic void writeToStaticRangeB(input Mword adr, input Mbyte val);
-        PageWriter#(Mbyte, 1)::writeTyped(content, adr, val);
-    endfunction
-
-
-    function automatic void writeToUncachedRangeW(input Mword adr, input Mword val);
-        PageWriter#(Word, 4, UNCACHED_BASE)::writeTyped(uncachedArea, adr, val);
-    endfunction
-
-    function automatic void writeToUncachedRangeB(input Mword adr, input Mbyte val);
-        PageWriter#(Mbyte, 1, UNCACHED_BASE)::writeTyped(uncachedArea, adr, val);
-    endfunction
-
-
-    function automatic Mword readWordStatic(input Mword adr);
-        return PageWriter#(Word, 4)::readTyped(content, adr);
-    endfunction
-
-    function automatic Mword readByteStatic(input Mword adr);
-        return Mword'(PageWriter#(Mbyte, 1, UNCACHED_BASE)::readTyped(content, adr));
-    endfunction
-
-
-    function automatic Mword readWordUncached(input Mword adr);
-        return PageWriter#(Word, 4)::readTyped(uncachedArea, adr);
-    endfunction
-
-    function automatic Mword readByteUncached(input Mword adr);
-        return Mword'(PageWriter#(Mbyte, 1, UNCACHED_BASE)::readTyped(uncachedArea, adr));
-    endfunction
+    ////////////////////////////////////
 
 
 
 
-
-    function automatic void writeToDynamicRangeW(input Mword adr, input Mword val);
-        localparam int ACCESS_SIZE = 4;
-        
-        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-        PhysicalAddressLow physLow = adr % BLOCK_SIZE;
-
-        Mbyte wval[ACCESS_SIZE] = {>>{val}};
-        filledBlocks[physBlockBase][physLow +: ACCESS_SIZE] = wval;
-    endfunction
-
-    function automatic void writeToDynamicRangeB(input Mword adr, input Mbyte val);
-        localparam int ACCESS_SIZE = 1;
-        
-        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-        PhysicalAddressLow physLow = adr % BLOCK_SIZE;
-
-        Mbyte wval[ACCESS_SIZE] = {>>{val}};
-        filledBlocks[physBlockBase][physLow +: ACCESS_SIZE] = wval;
-    endfunction
-
-
-
-    task automatic doWrite(input MemWriteInfo wrInfo);
-        Mword adr = wrInfo.adr;
-        Mword val = wrInfo.value;
-
-        if (!wrInfo.req) return;
-
-        if (wrInfo.uncached) begin
-            uncachedCounter = 15;
-            uncachedBusy = 1;
-            if (wrInfo.size == SIZE_1) writeToUncachedRangeB(adr, val);
-            if (wrInfo.size == SIZE_4) writeToUncachedRangeW(adr, val);
-        end
-        else if (isStaticDataRange(adr)) begin
-            if (wrInfo.size == SIZE_1) writeToStaticRangeB(adr, val);
-            if (wrInfo.size == SIZE_4) writeToStaticRangeW(adr, val);
-        end
-        else begin 
-            if (wrInfo.size == SIZE_1) writeToDynamicRangeB(adr, val);
-            if (wrInfo.size == SIZE_4) writeToDynamicRangeW(adr, val);
-        end
-    
-    endtask
-
-
-    function automatic logic isPhysPresent(input Mword adr);
-        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-        return isUncachedRange(adr) || isStaticDataRange(adr) || filledBlocks.exists(physBlockBase);
-    endfunction
-
-    function automatic logic isPhysPending(input Mword adr);
-        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-        return blockFillCounters.exists(physBlockBase);
-    endfunction
-    
-
-    function automatic logic isTlbPresent(input Mword adr);
-        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
-        return isStaticTlbRange(adr) || filledMappings.exists(pageBase);
-    endfunction
-
-    function automatic logic isTlbPending(input Mword adr);
-        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
-        return mappingFillCounters.exists(pageBase);
-    endfunction
-
-
-
+    /////////////////////////////////////////////////////////////////////////////
+    // General read functions
     function automatic Mword readFromUncachedRange(input Mword adr, input AccessSize size);
         if (size == SIZE_1) return readByteUncached(adr);
         else if (size == SIZE_4) return readWordUncached(adr);
@@ -352,55 +383,64 @@ module DataL1(
 
         return 'x;
     endfunction
+    
+    
+    task automatic doWrite(input MemWriteInfo wrInfo);
+        Mword adr = wrInfo.adr;
+        Mword val = wrInfo.value;
+
+        if (!wrInfo.req) return;
+
+        if (wrInfo.uncached) begin
+            uncachedCounter = 15;
+            uncachedBusy = 1;
+            if (wrInfo.size == SIZE_1) writeToUncachedRangeB(adr, val);
+            if (wrInfo.size == SIZE_4) writeToUncachedRangeW(adr, val);
+        end
+        else if (isStaticDataRange(adr)) begin
+            if (wrInfo.size == SIZE_1) writeToStaticRangeB(adr, val);
+            if (wrInfo.size == SIZE_4) writeToStaticRangeW(adr, val);
+        end
+        else begin 
+            if (wrInfo.size == SIZE_1) writeToDynamicRangeB(adr, val);
+            if (wrInfo.size == SIZE_4) writeToDynamicRangeW(adr, val);
+        end
+    
+    endtask
+
+    ///////////////////////////////
 
 
-    function automatic Mword readWordDynamic(input DataBlock block, input int offset);
-        localparam int ACCESS_SIZE = 4;
 
-        Mbyte chosenWord[ACCESS_SIZE] = block[offset +: ACCESS_SIZE];
-        Mword wval = {>>{chosenWord}};
-
-        return (wval);
-    endfunction
-
-    function automatic Mword readByteDynamic(input DataBlock block, input int offset);
-        localparam int ACCESS_SIZE = 1;
-
-        Mbyte chosenWord[ACCESS_SIZE] = block[offset +: ACCESS_SIZE];
-        Mbyte wval = {>>{chosenWord}};
-
-        return (wval);
-    endfunction
-
-
-    function automatic void allocInDynamicRange(input Mword adr);
-        Mword physBlockBase = (adr/BLOCK_SIZE)*BLOCK_SIZE;
-        //DataBlock block = '{default: 0};
-        
-        filledBlocks[physBlockBase] = '{default: 0};            
-    endfunction
-
-    function automatic void allocInTlb(input Mword adr);
-        Translation DUMMY; 
-        Mword pageBase = (adr/PAGE_SIZE)*PAGE_SIZE;
-        
-        filledMappings[pageBase] = DUMMY;            
-    endfunction
-       
+    task automatic handleFills();
+        handleBlockFills();
+        handleTlbFills();
+            
+        handleUncachedData();
+    endtask
 
     task automatic handleReads();
+        readOut <=      '{default: EMPTY_DATA_CACHE_OUTPUT};
+        accesses <=     '{default: DEFAULT_ACCESS_INFO};
+        translations <= '{default: DEFAULT_TRANSLATION};
+
         foreach (readReqs[p]) begin
             Mword vadr = readReqs[p].adr;
 
-            if ($isunknown(vadr) || !readReqs[p].active) begin
-                readOut[p] <= EMPTY_DATA_CACHE_OUTPUT;
-                accesses[p] <= DEFAULT_ACCESS_INFO;
-                translations[p] <= DEFAULT_TRANSLATION;
+            if (!readReqs[p].active) begin
+//                readOut[p] <= EMPTY_DATA_CACHE_OUTPUT;
+//                accesses[p] <= DEFAULT_ACCESS_INFO;
+//                translations[p] <= DEFAULT_TRANSLATION;
+                continue;
+            end
+            else if ($isunknown(vadr)) begin
+                
+                continue;
             end
             else begin
                 AccessInfo acc = analyzeAccess(vadr, readReqs[p].size);
                 Translation tr = translateAddress(vadr);
-                //PhysicalAddressHigh wayTag = tagsForWay[acc.block];
+                PhysicalAddressHigh wayTag = tagsForWay[acc.block];
                
                 DataCacheOutput thisResult = doReadAccess(acc, tr);
 
@@ -445,7 +485,7 @@ module DataL1(
                 uncachedReads[0].data = 'x;
                 uncachedReads[0].adr = 'x;
             end
-            else if (tr.phys <= $size(content)) // Read from small array
+            else if (tr.phys <= $size(staticContent)) // Read from small array
                 res.data = readFromStaticRange(tr.phys, aInfo.size);
             else
                 res.data = readFromDynamicRange(tr.phys, aInfo.size);
