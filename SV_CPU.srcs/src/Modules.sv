4e84fa09a77bfe609c94ea2525ab34fd10654148
fraktal-luk <fraktal3725@gmail.com>
2025-09-23 21:21:36 +0200

    Changing to many-cycle stage, corrections with flushing

--- SV_CPU.srcs/src/Modules.sv
+++ SV_CPU.srcs/src/Modules.sv
@@ -256,6 +256,9 @@ module DividerSubpipe#(
     input EventInfo lateEventInfo,
     input UopPacket opP
 );
+
+    localparam integer CONST_CYCLES = 9;
+
     UopPacket p0, p1 = EMPTY_UOP_PACKET,
                 pE0 = EMPTY_UOP_PACKET, pE1 = EMPTY_UOP_PACKET, pE2 = EMPTY_UOP_PACKET, pE3 = EMPTY_UOP_PACKET,
                 pE4 = EMPTY_UOP_PACKET, pE5 = EMPTY_UOP_PACKET, pE6 = EMPTY_UOP_PACKET, pE7 = EMPTY_UOP_PACKET, 
@@ -266,9 +269,18 @@ module DividerSubpipe#(
                 pD0_E, pD1_E;
     UopPacket stage0, stage0_E;
 
-    //assign stage0 = pE0;
-    assign stage0_E = pE11_E;//pE0_E;
+    UopPacket mainStage = EMPTY_UOP_PACKET,
+              outputStage0 = EMPTY_UOP_PACKET, // corresponds to pE{n} where n = CONST_CYCLES
+              outputStage1 = EMPTY_UOP_PACKET,
+              outputStage2 = EMPTY_UOP_PACKET,
+              outputStage0_E, outputStage1_E, outputStage2_E, 
+              mainStage_E;
+    integer cnt = -1;
 
+
+    //assign stage0 = pE0;
+    assign stage0_E = //pE11_E;//pE0_E;
+                      outputStage2_E;
     assign p0 = opP;
 
     always @(posedge AbstractCore.clk) begin
@@ -282,22 +294,55 @@ module DividerSubpipe#(
             pE6 <= (tickP(pE5));
             pE7 <= (tickP(pE6));
             pE8 <= (tickP(pE7));
+            
             pE9 <= (tickP(pE8));
             pE10 <= (tickP(pE9));
             pE11 <= (tickP(pE10));        
-        pD0 <= tickP(pE11);
+        pD0 <= tickP(//pE11);
+                     outputStage2);
         pD1 <= tickP(pD0);
     end
+
+
+
+    always @(posedge AbstractCore.clk) begin
+        
+        if (p1_E.active) begin
+            mainStage <= performRegularE0(p1_E); // New uop enters
+            cnt <= 0;
+        end
+        else if (cnt == CONST_CYCLES-1) begin  // End of work for current uop
+            mainStage <= EMPTY_UOP_PACKET;
+            cnt <= -1;
+        end
+        else if (mainStage_E.active) begin
+            cnt <= cnt + 1;
+        end
+        else begin
+            mainStage <= EMPTY_UOP_PACKET;
+            cnt <= -1;
+        end
+        
+        
+        if (cnt == CONST_CYCLES-1) outputStage0 <= tickP(mainStage);
+        else outputStage0 <= EMPTY_UOP_PACKET;
+        
+        
+        outputStage1 <= tickP(outputStage0);
+        outputStage2 <= tickP(outputStage1);
+    end
+
 
     logic lock = 0;
     logic empty, allowIssue, opSelected;
 
     assign opSelected = IS_FP ? theIssueQueues.fdivQueue.anySelected && theIssueQueues.fdivQueue.allow : theIssueQueues.dividerQueue.anySelected && theIssueQueues.dividerQueue.allow;
 
-    assign empty = !(p0.active || p1.active ||  
-                    pE0.active ||  pE1.active ||  pE2.active ||  pE3.active ||
-                    pE4.active ||  pE5.active ||  pE6.active ||  pE7.active ||
-                    pE8.active ||  pE9.active ||  pE10.active ||  pE11.active );
+    assign empty = !(p0.active || p1.active || mainStage.active || outputStage0.active || outputStage1.active || outputStage2.active ||
+                        pE0.active ||  pE1.active ||  pE2.active ||  pE3.active ||
+                        pE4.active ||  pE5.active ||  pE6.active ||  pE7.active ||
+                        pE8.active ||  pE9.active ||  pE10.active ||  pE11.active
+                    );
 
     // allow signal for divider IQ
     always @(posedge AbstractCore.clk) begin
@@ -313,18 +358,25 @@ module DividerSubpipe#(
     assign p0_E = effP(p0);
     assign p1_E = effP(p1);
     
-    assign pE0_E = effP(pE0);
-    assign pE1_E = effP(pE1);
-    assign pE2_E = effP(pE2);
-    assign pE3_E = effP(pE3);
-    assign pE4_E = effP(pE4);
-    assign pE5_E = effP(pE5);
-    assign pE6_E = effP(pE6);
-    assign pE7_E = effP(pE7);
-    assign pE8_E = effP(pE8);
-    assign pE9_E = effP(pE9);
-    assign pE10_E = effP(pE10);
-    assign pE11_E = effP(pE11);
+        assign pE0_E = effP(pE0);
+        assign pE1_E = effP(pE1);
+        assign pE2_E = effP(pE2);
+        assign pE3_E = effP(pE3);
+        assign pE4_E = effP(pE4);
+        assign pE5_E = effP(pE5);
+        assign pE6_E = effP(pE6);
+        assign pE7_E = effP(pE7);
+        assign pE8_E = effP(pE8);
+    
+        assign pE9_E = effP(pE9);
+        assign pE10_E = effP(pE10);
+        assign pE11_E = effP(pE11);
+
+    always_comb mainStage_E = effP(mainStage);
+
+    assign outputStage0_E = effP(outputStage0);
+    assign outputStage1_E = effP(outputStage1);
+    assign outputStage2_E = effP(outputStage2);
     
     assign pD0_E = effP(pD0);
 
@@ -339,9 +391,12 @@ module DividerSubpipe#(
 //    };
     
     assign image_E = '{
-        -2: pE9_E,
-        -1: pE10_E,
-        0: pE11_E,
+//        -2: pE9_E,
+//        -1: pE10_E,
+//        0: pE11_E,
+            -2: outputStage0_E,
+            -1: outputStage1_E,
+            0: outputStage2_E,
         1: pD0_E,
         default: EMPTY_FORWARDING_ELEMENT
     };
--- zzz.do
+++ zzz.do
@@ -2,3 +2,6 @@
 profile on
 profile off
 #restart -force
+
+add wave -position insertpoint sim:/ArchDesc0/core/theExecBlock/divider/*
+add wave -position insertpoint sim:/ArchDesc0/core/theExecBlock/fdiv/*
